79474a110f17ec39b5243a4fbc247de6bc11af2b
Vishesh <guptavishesh2@gmail.com>
2024-12-16 19:11:28 +0530

    Fixed the solution

--- 27_Vowels.js
+++ 27_Vowels.js
@@ -2,11 +2,12 @@ const solve = (input) => {
     let inputArr = input.split('\n');
     let vowels = ['a', 'e', 'i', 'o', 'u'];
     let n = parseInt(inputArr.shift())
+    console.log(typeof n);
 
     for (let i = 0; i < n; i++) {
         let count = 0;
         for (let j = 0; j < inputArr[i].length; j++) {
-            if (vowels.includes(inputArr[i][j])) {
+            if (vowels.includes(inputArr[i][j].toLowerCase())) {
                 count++;
             }
         }
