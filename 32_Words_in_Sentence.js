323db735a49aba9ed1fa5090219e1e8ce763d529
Vishesh <guptavishesh2@gmail.com>
2024-12-18 18:51:46 +0530

    Words in sentence

---
+++ 32_Words_in_Sentence.js
@@ -0,0 +1,8 @@
+const solve = (input) => {
+    // Write your code here\
+    const words = input.split(" ");
+    console.log(words.length);
+
+}
+
+solve(`The quick brown fox jumped over the lazy dog.`)
