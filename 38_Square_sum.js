8abb68dd1e1a9f8afde1cb16c9f257bde9d3f4df
Vishesh <guptavishesh2@gmail.com>
2024-12-18 18:53:10 +0530

    Square sum

---
+++ 38_Square_sum.js
@@ -0,0 +1,19 @@
+const solve = (input) => {
+    // Write your code here
+    const inputArr = input.split("\n");
+    const length = parseInt(inputArr.shift());
+
+    for (let i = 0; i < length; i++) {
+        let sum = 0;
+        let lineArr = inputArr[i].split("").map(Number)
+        for (let j = 0; j < lineArr.length; j++) {
+            sum += lineArr[j] * lineArr[j];
+        }
+        console.log(sum)
+    }
+
+}
+
+solve(`2
+12
+13246`)
